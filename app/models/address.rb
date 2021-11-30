e552f9d0efd9c3e9651c2fbaedbfee7ad1b42ebf
Soma1412 <syama395@mtroyal.ca>
2021-11-30 19:36:27 +0900

    バリデーションとアソシエーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  validates :post_cord,          presence: true
+  validates :prefecture_id,      presence: true
+  validates :city,               presence: true
+  validates :address,            presence: true
+  varidates :phhone_number,      presence: true
+
+  belongs_to :purchase
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :item_name,           presence: true
+  validates :explain,             presence: true
+  validates :category_id,         presence: true
+  validates :condition_id,        presence: true
+  validates :delivery_cost_id,    presence: true
+  validates :delivery_day_id,     presence: true
+  validates :delivery_place_id,   presence: true
+  varidates :price,               presence: true
+
+  belongs_to :user
+  has_one    :purchase
+  
 end
