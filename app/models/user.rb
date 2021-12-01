462b1a43804f3499d160ef8a2b589c1b33b9f13a
Soma1412 <syama395@mtroyal.ca>
2021-12-01 16:40:10 +0900

    user model を作成した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-        validates :nickname,         presence: true
+        validates :nickname,         presence: true, length: { maximum: 6 }
         validates :kanji_last_name,  presence: true
-        varidates :kanji_first_name, presence: true
+        validates :kanji_first_name, presence: true
         validates :kana_last_name,   presence: true
         validates :kana_first_name,  presence: true
         validates :birth_date,       presence: true 
 
         has_many  :item
         has_many  :purchase
-         
+
 end
