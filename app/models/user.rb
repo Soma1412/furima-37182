df5da9c0202c60471c4d884ce5e25abba2c913f9
Soma1412 <syama395@mtroyal.ca>
2021-12-02 18:09:17 +0900

    新規登録、ログイン、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
         validates :nickname,         presence: true, length: { maximum: 6 }
         validates :kanji_last_name,  presence: true
         validates :kanji_first_name, presence: true
