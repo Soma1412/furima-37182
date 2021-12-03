20b6df3dc2c70562b752e8aeeb296be63ec08c6f
Soma1412 <syama395@mtroyal.ca>
2021-12-03 17:21:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  validates :item_name,           presence: true
+  validates :explain,             presence: true
+  validates :category_id,         presence: true
+  validates :condition_id,        presence: true
+  validates :delivery_cost_id,    presence: true
+  validates :delivery_day_id,     presence: true
+  validates :delivery_place_id,   presence: true
+  validates :price,               presence: true
+
+  belongs_to :user
+  has_one    :purchase
   has_one_attached :image
 end
