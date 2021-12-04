91ead22929de9e20d2418dee1379a2f979b44f98
Soma1412 <syama395@mtroyal.ca>
2021-12-04 21:54:30 +0900

    active hash の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,16 +7,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def delete
   end
 
   def new
-    @item = Item.all
-  end
-
-  def create
+    @item = Item.new
   end
 
   private
--- app/models/delivery_day_id.rb
+++ app/models/delivery_day_id.rb
@@ -1,3 +1,7 @@
 class Delivery_day < ActiveHash::Base
 belongs_to :item
+  { id: 0, name: '--' },
+  { id: 1, name: '1~2で発送' },
+  { id: 2, name: '2~3で発送' },
+  { id: 3, name: '4~7で発送' },
 end
