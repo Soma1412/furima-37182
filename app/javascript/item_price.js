244c9e7373d1f844796122486e76e4aa11005ef6
Soma1412 <syama395@mtroyal.ca>
2021-12-05 19:16:58 +0900

    販売利益の記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,18 @@
  window.addEventListener('load',() => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    Math.floor(inputValue * 0.1)
+    console.log(Math.floor(inputValue * 0.1))
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+    const addProfit = document.getElementById("profit")
+    Math.floor(inputValue - addTaxDom.innerHTML )
+    console.log(inputValue - addTaxDom.innerHTML )
+    addProfit.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML )
   })
  })
 
