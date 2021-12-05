32d29096fa815ec4e540f794a4eebe7af8d3fdb6
Soma1412 <syama395@mtroyal.ca>
2021-12-05 17:53:51 +0900

    jsイベント発火

---
+++ app/javascript/item_price.js
@@ -0,0 +1,9 @@
+ window.addEventListener('load',() => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
+ })
+
+
+
