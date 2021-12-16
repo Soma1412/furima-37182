3b89b69677fdd74dba0681df4768a9fe3b14bf0b
Soma1412 <syama395@mtroyal.ca>
2021-12-16 18:48:00 +0900

    モデル単体テスト修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,17 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :item_id, :token, :post_code, :delivery_place_id, :city, :address, :building, :phone_number, :purchase, :user_id 
+
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
     validates :city, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     # 半角英字数字のみ許可する
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input crrectly" }
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "Input only number"}
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid."}
     # delivery_place_idは0以外選択できない
     validates :delivery_place_id,    numericality: { other_than: 0 }
     # 空の値は保存できない
     validates :address
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   def save
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
-
 RSpec.describe PurchaseAddress, type: :model do
     before do
-      @purchase_address = FactoryBot.build(:purchase_address)
+      sleep 0.1
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id )
     end
   
   describe '商品の購入' do
@@ -11,6 +13,12 @@ RSpec.describe PurchaseAddress, type: :model do
       it '商品購入に必要な情報が正しく入力されている' do
         expect(@purchase_address).to be_valid
       end
+
+      it  "建物名が空でも購入できる" do
+        @purchase_address.building = ''
+        expect(@purchase_address).to be_valid
+      end
+
     end
   
     context '商品が購入できないとき' do
@@ -54,14 +62,46 @@ RSpec.describe PurchaseAddress, type: :model do
       it '電話番号が半角数字が入力されてないときは購入できない' do
         @purchase_address.phone_number = 'aaaaaaa'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid.")
       end
 
       it "tokenが空だと購入できないこと" do
         @purchase_address.token = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Token can't be blank") 
-      end    
+      end
+
+
+    it '都道府県に「---」が選択されている場合は購入できない' do
+      @purchase_address.delivery_place_id = '0'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Delivery place must be other than 0")
+    end
+
+  it '電話番号が9桁以下では購入できない' do
+      @purchase_address.phone_number = '1'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Phone number is invalid.")
+    end
+
+  it "電話番号が12桁以上では購入できない" do
+    @purchase_address.phone_number = '111111111111111'
+    @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Phone number is invalid.")
+    end
+
+  it "userが紐付いていなければ購入できない" do
+    @purchase_address.user_id = nil
+    @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("User can't be blank")
+  end
+
+  it'itemが紐付いていなければ購入できない' do
+    @purchase_address.item_id = nil
+    @purchase_address.valid?
+    expect(@purchase_address.errors.full_messages).to include("Item can't be blank")
+  end
+     
     end
   end
 end
